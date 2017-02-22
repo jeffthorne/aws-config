2378bc1bacd016c47d937053cc9a59f245dc36ad
Jeff Thorne <jeff.thorne@gmail.com>
2017-02-22 12:50:05 -0500

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import json
-from manager import Manager
-
-dsm = Manager("awsconfig", "Password11!", "ACME Corp")
-
-print dsm.does_aws_host_have_malware_turned_on("i-009476583069cf714").malware_protection_on
